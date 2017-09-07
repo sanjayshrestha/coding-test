0334b19cb669813b337243cabc34183a685a5128
Anji Reddy <anjireddyinjas@gmail.com>
2017-09-07 00:46:45 -0400

    Anji Reddy - Assignment Submission

--- src/main/java/com/monsanto/mbt/Shipment.java
+++ src/main/java/com/monsanto/mbt/Shipment.java
@@ -1,9 +1,47 @@
 package com.monsanto.mbt;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Shipment {
 
+	public List<Widget> sortShipmentByDate(List<Widget> widgetList) {
 
+		Comparator<Widget> dateComparator = new Comparator<Widget>() {
 
+			public int compare(Widget obj1, Widget obj2) {
 
+				if (obj1.getProductionDate().before(obj2.getProductionDate())) {
+					return -1;
+				} else if (obj1.getProductionDate().after(obj2.getProductionDate())) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+		};
+		Collections.sort(widgetList, dateComparator);
+
+		List<Widget> sortedWidgetList = widgetList.subList(0, 10);
+
+		return sortedWidgetList;
+	}
+
+	public List<Widget> sortShipmentByColor(List<Widget> widgetList) {
+		Comparator<Widget> colorComparator = new Comparator<Widget>() {
+
+			public int compare(Widget obj1, Widget obj2) {
+
+				return obj1.getColor().compareTo(obj2.getColor());
+			}
+		};
+		Collections.sort(widgetList, colorComparator);
+		
+		List<Widget> sortedWidgetList = widgetList.subList(0, 10);
+
+		return sortedWidgetList;
+
+	}
 
 }
--- src/test/java/com/monsanto/mbt/ShipmentTest.java
+++ src/test/java/com/monsanto/mbt/ShipmentTest.java
@@ -1,24 +1,64 @@
 package com.monsanto.mbt;
 
 
-import org.junit.Test;
-
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ShipmentTest {
+	Shipment shipment;
+	List<Widget> widgets;
+	List<Date> sortedDates;
 
-    @Test
-    public void testShipment_Sorted_By_Color(){
-        //Implement your solution to get list of shipments
+	@Before
+	public void init() {
+		shipment = new Shipment();
+		widgets = WidgetUtils.getSampleWidgets();
+		sortedDates = new ArrayList<Date>();
+		for (Widget widget: widgets) {
+			sortedDates.add(widget.getProductionDate());
+		}
 
-    }
-
-    @Test
-    public void testShipment_Sorted_By_Date(){
-        //Implement your solution to get list of shipments
+		Collections.sort(sortedDates);
+	}
 
 
-    }
-
-
+	@Test
+	public void testShipment_Sorted_By_Color() {
+		List<Widget> sortedWidgetList = shipment.sortShipmentByColor(widgets);
+		
+		//Constraints on shipment creation is that a shipment can hold a maximum of 10 Widgets.
+		assertEquals(10, sortedWidgetList.size());
+		
+		for(int i=0; i< sortedWidgetList.size(); i++) {
+			if (i < sortedWidgetList.size() - 1 ) {
+				assertTrue(sortedWidgetList.get(i).getColor().equals(sortedWidgetList.get(i +1 ).getColor()) 
+						|| (sortedWidgetList.get(i).getColor().compareTo(sortedWidgetList.get(i + 1).getColor()) < 0) );
+			}
+		}
+	}
+	
+	@Test
+	public void testShipment_Sorted_By_Date() {
+		List<Widget> sortedWidgetList = shipment.sortShipmentByDate(widgets);
+		List<Date> limitedSortedDates = sortedDates.subList(0, 10);
+		
+		//Constraints on shipment creation is that a shipment can hold a maximum of 10 Widgets.
+		assertEquals(10, sortedWidgetList.size());
+		
+		for(int i=0; i< sortedWidgetList.size(); i++) {
+			assertEquals(limitedSortedDates.get(i), sortedWidgetList.get(i).getProductionDate());
+			if (i < sortedWidgetList.size() - 1 ) {
+				assertTrue(sortedWidgetList.get(i).getProductionDate().equals(sortedWidgetList.get(i +1 ).getProductionDate()) 
+						|| sortedWidgetList.get(i).getProductionDate().before(sortedWidgetList.get(i + 1).getProductionDate()));
+			}
+		}
+	}
 }
