67c9c6563ffd269d542323d5eda4429f78f250f1
Ashalatha-26 <45114776+Ashalatha-26@users.noreply.github.com>
2018-11-17 13:45:46 +0530

    Committing Shipment Assessment

--- src/main/java/com/monsanto/mbt/Widget.java
+++ src/main/java/com/monsanto/mbt/Widget.java
@@ -1,40 +1,67 @@
 package com.monsanto.mbt;
 
+import java.util.Comparator;
 import java.util.Date;
 
-
 public class Widget {
-    private int serialNumber;
-    private String color;
-    private Date productionDate;
+	private int serialNumber;
+	private String color;
+	private Date productionDate;
 
-    public Widget(int serialNumber, String color, Date productionDate) {
-        this.serialNumber = serialNumber;
-        this.color = color;
-        this.productionDate = productionDate;
-    }
+	public Widget(int serialNumber, String color, Date productionDate) {
+		this.serialNumber = serialNumber;
+		this.color = color;
+		this.productionDate = productionDate;
+	}
 
-    public int getSerialNumber() {
-        return serialNumber;
-    }
+	public int getSerialNumber() {
+		return serialNumber;
+	}
 
-    public void setSerialNumber(int serialNumber) {
-        this.serialNumber = serialNumber;
-    }
+	public void setSerialNumber(int serialNumber) {
+		this.serialNumber = serialNumber;
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+	public void setColor(String color) {
+		this.color = color;
+	}
 
-    public Date getProductionDate() {
-        return productionDate;
-    }
+	public Date getProductionDate() {
+		return productionDate;
+	}
 
-    public void setProductionDate(Date productionDate) {
-        this.productionDate = productionDate;
-    }
+	public void setProductionDate(Date productionDate) {
+		this.productionDate = productionDate;
+	}
+
+	public static Comparator<Widget> colorComp = new Comparator<Widget>() {
+
+		@Override
+		public int compare(Widget widget1, Widget widget2) {
+
+			return widget1.getColor().toUpperCase()
+					.compareTo(widget2.getColor().toUpperCase());
+		}
+	};
+
+	public static Comparator<Widget> ProdDateComp = new Comparator<Widget>() {
+
+		@Override
+		public int compare(Widget widjet1, Widget widjet2) {
+
+			if (widjet1.getProductionDate().before(widjet2.getProductionDate())) {
+				return -1;
+			} else if (widjet1.getProductionDate().after(
+					widjet2.getProductionDate())) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+
+	};
 }
